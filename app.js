9f8b6b4104fb474f1e24a468c232982436a0a406
DucLUT <duc@LAPTOP-JIR1VD1O>
2023-11-25 21:21:00 +0200

    mm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
-import { sql } from "./database/database.js";
+import * as addressService from "./services/addressService.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -10,28 +10,45 @@ const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const data = {
-  count: 0,
+const redirectTo = (path) => {
+  return new Response(`Redirecting to ${path}.`, {
+    status: 303,
+    headers: {
+      "Location": path,
+    },
+  });
 };
 
+const deleteMessage = async (request) => {
+  const url = new URL(request.url);
+  const part = url.pathname.split("/");
+  const id = part[2]
+  await addressService.deleteById(id);
+  return redirectTo("/");
+}
+
+const saveMessage = async (request) => {
+  const formData = await request.formData();
+  const sender = formData.get("sender");
+  const message = formData.get("message");
+  await addressService.create(sender,message);
+  return redirectTo("/");
+}
+const listAddresses = async () => {
+  const data = {
+    addresses: await addressService.findAll(),
+  };
+  return new Response(await renderFile("count.eta", data), responseDetails);
+  
+}
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/count") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
-  }
-
-  if (url.pathname === "/addresses") {
-    const rows = await sql`SELECT COUNT(*) as count FROM addresses`;
-    let rowCount = -42;
-    if (rows.length > 0) {
-      rowCount = rows[0].count;
-    }
-
-    return new Response(`Total rows: ${rowCount}`);
-  }
-
-  return new Response("Hello you!");
-};
+  if (request.method === "POST" && url.pathname.startsWith("/delete/")) {
+    return await deleteMessage(request);
+  }else if (request.method === "POST") {
+    return await saveMessage(request);
+  }else{
+    return await listAddresses();
+}};
 
 serve(handleRequest, { port: 7777 });
